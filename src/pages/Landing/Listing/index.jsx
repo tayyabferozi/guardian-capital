f1b26e264e75bd5476ea419060a669611c68a464
Tayyab Ferozi <ferozitayyab@gmail.com>
2022-08-17 23:51:47 +0500

    adds sorting

--- src/pages/Landing/Listing/index.jsx
+++ src/pages/Landing/Listing/index.jsx
@@ -600,6 +600,40 @@ const Listing = () => {
     });
   }, []);
 
+  useEffect(() => {
+    setTimeout(function () {
+      $("th")
+        .off("click")
+        .on("click", function () {
+          var table = $(this).parents("table").eq(0);
+          var rows = table
+            .find("tr:gt(0)")
+            .toArray()
+            .sort(comparer($(this).index()));
+          this.asc = !this.asc;
+          console.log(this.asc);
+          if (!this.asc) {
+            rows = rows.reverse();
+          }
+          for (var i = 0; i < rows.length; i++) {
+            table.append(rows[i]);
+          }
+        });
+      function comparer(index) {
+        return function (a, b) {
+          var valA = getCellValue(a, index),
+            valB = getCellValue(b, index);
+          return $.isNumeric(valA) && $.isNumeric(valB)
+            ? valA - valB
+            : valA.toString().localeCompare(valB);
+        };
+      }
+      function getCellValue(row, index) {
+        return $(row).children("td").eq(index).text();
+      }
+    }, 500);
+  }, []);
+
   return (
     <div id="listing">
       <div className="filter-container short-section mb-0">
